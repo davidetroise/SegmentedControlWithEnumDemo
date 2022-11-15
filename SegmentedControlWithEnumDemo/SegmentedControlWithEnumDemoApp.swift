64816aa38b85d8a2bd525d37dcca8e80416a0d5d
Davide Troise <davide@troise.net>
2022-11-15 16:33:28 +0100

    Initial Commit

---
+++ SegmentedControlWithEnumDemo/SegmentedControlWithEnumDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  SegmentedControlWithEnumDemoApp.swift
+//  SegmentedControlWithEnumDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControlWithEnumDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
